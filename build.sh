64ef5cc2e8c25b673e90a36213b6ef8ac51ab363
allys br <abr94551@gmail.com>
2026-02-13 15:09:21 +0530

    updated

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build -t hls-browser -f Dockerfile.chromium-hls .
+#docker build -t hls-browser -f Dockerfile.chromium-hls .
+docker build --no-cache -t hls-browser -f Dockerfile.chromium-hls .
